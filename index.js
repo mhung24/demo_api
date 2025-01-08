dcad2803505bab4a3a931e14aea39f826e20a73e
mhung24 <hung123mqtb@gmail.com>
2025-01-08 21:58:26 +0700

    update

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let api = require("../app/routes/api.js");
+// let api = require("./routes/api.js");
+let dataApi = require("./app/controller/dataApi.js");
+
+app.get("/api", dataApi.getList);
 let bdparser = require("body-parser");
 app.use(express.json());
-app.use("/", api);
+// app.use("/", api);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
