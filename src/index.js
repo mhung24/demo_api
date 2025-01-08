2d9c89df84766548c03d3e0918195d22ac52dd6b
mhung24 <hung123mqtb@gmail.com>
2025-01-08 21:38:48 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let api = require("./app/routes/api.js");
+let api = require("../app/routes/api.js");
 let bdparser = require("body-parser");
 app.use(express.json());
 app.use("/", api);
