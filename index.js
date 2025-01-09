e30dd07c091a17089d68b03363aa32811256ae21
mhung24 <hung123mqtb@gmail.com>
2025-01-09 20:03:22 +0700

    tes1

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// // let api = require("./routes/api.js");
+// let dataApi = require("./app/controller/dataApi.js");
+
+// app.get("/api", dataApi.getList);
+// let bdparser = require("body-parser");
+// app.use(express.json());
+// // app.use("/", api);
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-// let api = require("./routes/api.js");
-let dataApi = require("./app/controller/dataApi.js");
-
-app.get("/api", dataApi.getList);
-let bdparser = require("body-parser");
-app.use(express.json());
-// app.use("/", api);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
